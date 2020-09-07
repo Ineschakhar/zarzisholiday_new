20353cf5ab20c1416c6aeb474a432a65e26e2a26
Ines Chakhar <ineschakhar@github.com>
2020-09-07 12:36:20 +0100

    commit .env email et reservationtype et appartementtype

--- src/Form/ReservationType.php
+++ src/Form/ReservationType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\Reservation;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ReservationType extends AbstractType
@@ -22,6 +23,13 @@ class ReservationType extends AbstractType
             ->add('NbrAdulte')
             ->add('NbrEnfant')
             ->add('tarifs')
+            ->add('status', ChoiceType::class, [
+                'choices' => [
+                    'New' => 'New',
+                    'Accepted' => 'Accepted',
+                    'Rejected' => 'Rejected'
+                ],
+            ])
 
             //->add('createdAt')
             //->add('updatedAt')
